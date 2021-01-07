050e61b8383bd87cdc5a3e04dc250bde2f32156f
DasolJong <dasoljong92@gmail.com>
2021-01-07 22:41:42 +0900

    w: kakao profile

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,20 @@
 <!--    <img alt="Vue logo" src="./assets/logo.png">-->
 <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
     <BookInfo />
+    <Kakao />
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue';
 import BookInfo from './components/BookInfo.vue';
+import Kakao from './components/Kakao.vue';
 
 export default {
   name: 'App',
   components: {
     BookInfo,
+    Kakao,
   }
 }
 </script>
---
+++ src/components/Kakao.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="kakaoProfileContainer">
+    <h1>My KaKao Profile</h1>
+    <button class="btn btn-info" @click="getKakaoProfile">프로필 정보 가져오기</button>
+    <div class="table-wrapper">
+      <table>
+        <thead>
+          <tr>
+            <td>NAME</td>
+            <td>Pic.</td>
+            <td>Nationality</td>
+          </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>{{}}</td>
+          <td>{{}}</td>
+          <td>{{}}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: "kakaoProfile",
+  data() {
+    return {
+      contents: []
+    }
+  },
+  methods: {
+    getKakaoProfile: function() {
+      axios.get('https://kapi.kakao.com/v1/api/talk/profile')
+          .then(res => {
+            // handle success
+            console.log(res);
+          })
+          .catch(err => {
+            // handle error
+            console.log(err);
+          })
+          .then(() => {
+            // always executed
+          });
+    }
+  }
+}
+</script>
+
+
+<style>
+.kakaoProfileContainer {
+  margin: 200px 0;
+}
+.kakaoProfileContainer h1{
+  padding-bottom: 20px;
+  border-bottom: 1px solid darkgrey;
+}
+.kakaoProfileContainer .btn-info {
+  margin-top: 20px;
+}
+</style>
