fe4f32aee7dfc10a5e70a34e7732e6a8ec74d411
DasolJong <dasoljong92@gmail.com>
2021-01-07 19:08:07 +0900

    w: layout completed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <AddressBook />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue';
+import AddressBook from './components/AddressBook.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AddressBook,
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 100px;
+  margin: 0 auto;
 }
 </style>
