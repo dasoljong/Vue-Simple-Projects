072df897f6a520a1c5ad5802fa662c28670a9fd3
DasolJong <dasoljong92@gmail.com>
2021-01-08 00:45:45 +0900

    w: random user added

---
+++ src/components/RandomUser.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="randomProfileContainer">
+    <h1>Random User</h1>
+    <button class="btn btn-info" @click="randomProfile">랜덤 유저 가져오기</button>
+    <div class="personalCardWrapper">
+      <ul>
+        <li v-bind:key="users">
+          <div><img :src="users[1].avatar" :alt="users[1].first_name"></div>
+          <span>{{users[1].first_name}} {{users[1].last_name}}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: "randomProfileContainer",
+  data() {
+    return {
+      users: [],
+      randomNum: 0,
+    }
+  },
+  methods: {
+    randomProfile: function() {
+      axios.get('https://reqres.in/api/users?page=2')
+          .then(res => {
+            // handle success
+            this.users = res.data.data
+            this.randomNum = Math.floor(Math.random() * this.users.length)
+          })
+          .catch(err => {
+            // handle error
+            console.log(err);
+          })
+          .then(() => {
+            // always executed
+          });
+    }
+  }
+}
+</script>
+
+
+<style>
+.randomProfileContainer {
+  margin: 200px 0;
+}
+.randomProfileContainer h1{
+  padding-bottom: 20px;
+  border-bottom: 1px solid darkgrey;
+}
+.randomProfileContainer .btn-info {
+  margin: 20px 0;
+}
+.randomProfileContainer ul {
+  width: 100%;
+  display: flex;
+  padding-left: unset;
+}
+.randomProfileContainer ul li {
+  display: none;
+  list-style: none;
+  width: 33%;
+}
+</style>
