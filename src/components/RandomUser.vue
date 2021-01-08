5f056aa20ffc798b5032010b2600b6a3ddfb82da
Dasol Jong <dasoljong92@gmail.com>
2021-01-08 16:46:07 +0900

    w: random user done;

--- src/components/RandomUser.vue
+++ src/components/RandomUser.vue
@@ -3,12 +3,8 @@
     <h1>Random User</h1>
     <button class="btn btn-info" @click="randomProfile">랜덤 유저 가져오기</button>
     <div class="personalCardWrapper">
-      <ul>
-        <li v-for="user in users" v-bind:key="user">
-          <div><img :src="user.avatar" :alt="user.first_name"></div>
-          <span>{{user.first_name}} {{user.last_name}}</span>
-        </li>
-      </ul>
+      <div><img :src="user.avatar" :alt="user.first_name"></div>
+      <span>{{user.first_name}} {{user.last_name}}</span>
     </div>
   </div>
 </template>
@@ -20,6 +16,7 @@ export default {
   name: "randomProfileContainer",
   data() {
     return {
+      user: [],
       users: [],
       randomNum: 0,
     }
@@ -29,8 +26,9 @@ export default {
       axios.get('https://reqres.in/api/users?page=2')
           .then(res => {
             // handle success
-            this.users = res.data.data
-            this.randomNum = Math.floor(Math.random() * this.users.length)
+            this.users = res.data.data // data 가져와서 users에 json으로 저장
+            this.randomNum = Math.floor(Math.random() * this.users.length) // random number 생성
+            this.user = this.users[this.randomNum] // user에 랜덤 프로필 지정해서 json으로 저장
           })
           .catch(err => {
             // handle error
@@ -56,13 +54,5 @@ export default {
 .randomProfileContainer .btn-info {
   margin: 20px 0;
 }
-.randomProfileContainer ul {
-  width: 100%;
-  display: flex;
-  padding-left: unset;
-}
-.randomProfileContainer ul li {
-  list-style: none;
-  width: 33%;
-}
+
 </style>
