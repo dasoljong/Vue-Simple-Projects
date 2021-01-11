b551cc9c5fe5d74f65aefc5f18607f4233decdc1
Dasol Jong <dasoljong92@gmail.com>
2021-01-11 17:03:58 +0900

    w: wrongd

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,37 @@
 <template>
   <div id="app">
-    <BookInfo />
-    <UserProfile />
-    <RandomUser />
-    <Weather />
+    <router-link to="/">Countdown</router-link>
+    <router-link to="book-info">Book Info</router-link>
+    <router-link to="random-user">Random User</router-link>
+    <router-link to="users">Users</router-link>
+    <router-link to="weather">Weather</router-link>
+    <router-view />
+
+    <!--    <BookInfo />-->
+<!--    <UserProfile />-->
+<!--    <RandomUser />-->
+<!--    <Weather />-->
+<!--    <CountdownTimer />-->
   </div>
 </template>
 
 <script>
-import BookInfo from "./components/BookInfo.vue";
-import UserProfile from "./components/UserProfile.vue";
-import RandomUser from "./components/RandomUser.vue";
-import Weather from "./components/Weather.vue";
-
-export default {
-  name: "App",
-  components: {
-    UserProfile,
-    BookInfo,
-    RandomUser,
-    Weather
-  }
-};
+// import BookInfo from "./components/BookInfo.vue";
+// import UserProfile from "./components/UserProfile.vue";
+// import RandomUser from "./components/RandomUser.vue";
+// import Weather from "./components/Weather.vue";
+// import CountdownTimer from "./components/CountdownTimer.vue";
+//
+// export default {
+//   name: "App",
+//   components: {
+//     UserProfile,
+//     BookInfo,
+//     RandomUser,
+//     Weather,
+//     CountdownTimer,
+//   },
+// };
 </script>
 
 <style>
---
+++ src/components/CountdownTimer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="countdownTimerContainer">
+    Countdown
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+//body {
+//  background-image: url("https://images.unsplash.com/photo-1534274867514-d5b47ef89ed7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2170&q=80");
+//  width: 100%;
+//  height: auto;
+//}
+</style>
