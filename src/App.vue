7a76ac7cfaadd2d8e85edbd8766c2f8cb9cbfc73
Dasol Jong <dasoljong92@gmail.com>
2021-01-11 17:05:39 +0900

    w;wrong

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,29 @@
 <template>
   <div id="app">
-    <router-link to="/">Countdown</router-link>
-    <router-link to="book-info">Book Info</router-link>
-    <router-link to="random-user">Random User</router-link>
-    <router-link to="users">Users</router-link>
-    <router-link to="weather">Weather</router-link>
-    <router-view />
-
-    <!--    <BookInfo />-->
-<!--    <UserProfile />-->
-<!--    <RandomUser />-->
-<!--    <Weather />-->
-<!--    <CountdownTimer />-->
+    <BookInfo />
+    <UserProfile />
+    <RandomUser />
+    <Weather />
   </div>
 </template>
 
 <script>
-// import BookInfo from "./components/BookInfo.vue";
-// import UserProfile from "./components/UserProfile.vue";
-// import RandomUser from "./components/RandomUser.vue";
-// import Weather from "./components/Weather.vue";
-// import CountdownTimer from "./components/CountdownTimer.vue";
-//
-// export default {
-//   name: "App",
-//   components: {
-//     UserProfile,
-//     BookInfo,
-//     RandomUser,
-//     Weather,
-//     CountdownTimer,
-//   },
-// };
+import BookInfo from "./components/BookInfo.vue";
+import UserProfile from "./components/UserProfile.vue";
+import RandomUser from "./components/RandomUser.vue";
+import Weather from "./components/Weather.vue";
+import CountdownTimer from "./components/CountdownTimer.vue";
+
+export default {
+  name: "App",
+  components: {
+    UserProfile,
+    BookInfo,
+    RandomUser,
+    Weather,
+    CountdownTimer,
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
-import VueRouter from "vue-router";
-
-Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
-
-const routes = [
-  {path: '/',
-    name}
-]
 
 new Vue({
   render: (h) => h(App),
