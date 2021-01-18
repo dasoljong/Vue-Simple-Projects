5c1763db87892d761e75ff14689e4df4eaf03c20
Dasol Jong <dasoljong92@gmail.com>
2021-01-18 20:14:34 +0900

    w: note app layout

---
+++ src/components/NoteApp.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="noteAppContainer"></div>
+</template>
+
+<script>
+export default {
+  name: "note-app",
+  data() {
+    return {};
+  },
+  methods: {},
+  created() {},
+};
+</script>
+
+<style lang="scss"></style>
